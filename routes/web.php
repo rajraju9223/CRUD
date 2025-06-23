6ea255d0418ffe674ed6ef95fdfb31c7bf1c4af2
rajuraj kalita <31398887+rajraju9223@users.noreply.github.com>
2025-06-23 05:55:19 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
     Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
     Route::post('/product', [ProductController::class, 'store'])->name('product.store');
     Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
